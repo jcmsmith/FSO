4df63ec85ab826dae8e3d33056c7bf5cf4b7ea39
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-04-16 01:18:47 -0700

    Exercises 2.2 & 2.3
    Display the total number of exercises using the reduce method

--- Part2/courseinfo/src/Components/Course.js
+++ Part2/courseinfo/src/Components/Course.js
@@ -3,10 +3,13 @@ import Header from "./Header";
 import Content from "./Content"
 
 const Course = ({ course }) => {
+    const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0)
+
     return (
     <div>
         <Header name={course.name} />
         <Content parts={course.parts} />
+        <strong>total of {totalExercises} exercises</strong>
     </div>
     ) 
 }
