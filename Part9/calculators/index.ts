bf3a3edd0284ba7082d0036db005bada0f5b77ec
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-05-18 21:23:50 -0700

    Exercise 9.7
    Add an endpoint to your app for the exercise calculator. It should be used by doing an HTTP POST request to endpoint exercises with the input in the request body

--- Part9/calculators/index.ts
+++ Part9/calculators/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 const app = express();
 
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
+
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
@@ -24,6 +27,36 @@ app.get("/bmi", (req, res) => {
   };
 
   return res.json(result);
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line
+  const target: number = req.body.target;
+
+  // eslint-disable-next-line
+  const daily_exercises: Array<number> = req.body.daily_exercises;
+
+  if (!target || !daily_exercises) {
+    res.status(400).json({ error: "parameters missing" });
+  }
+
+  if (
+    isNaN(target) ||
+    !isFinite(target) ||
+    !(daily_exercises instanceof Array)
+  ) {
+    res.status(400).json({ error: "malformed parameters" });
+  }
+
+  daily_exercises.forEach((hours) => {
+    if (isNaN(Number(hours)) || !isFinite(Number(hours))) {
+      res.status(400).json({ error: "malformed parameters" });
+    }
+  });
+
+  const result = calculateExercises(target, daily_exercises);
+
+  res.status(200).json(result);
 });
 
 const PORT = 3002;
