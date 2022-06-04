522d6749653070a1f02ee6a34f824736f528e19b
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-06-04 01:33:08 -0700

    Exercise 12.9
    Set up Redis for the project

--- Part12/part12-containers-applications/todo-app/todo-backend/util/config.js
+++ Part12/part12-containers-applications/todo-app/todo-backend/util/config.js
@@ -1,7 +1,7 @@
 const MONGO_URL =
   process.env.MONGO_URL ||
   "mongodb://the_username:the_password@localhost:3456/the_database";
-const REDIS_URL = process.env.REDIS_URL || undefined;
+const REDIS_URL = "redis://localhost:6378" || process.env.REDIS_URL;
 
 module.exports = {
   MONGO_URL, //: 'mongodb://the_username:the_password@localhost:3456/the_database',
