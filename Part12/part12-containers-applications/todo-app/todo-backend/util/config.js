fedfa985c8e4c5b3093ee23f5276777c50b07043
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-06-05 19:02:08 -0700

    Exercise 12.16
    Use volumes and Nodemon to enable the development of the todo app backend while it is running inside a container

--- Part12/part12-containers-applications/todo-app/todo-backend/util/config.js
+++ Part12/part12-containers-applications/todo-app/todo-backend/util/config.js
@@ -1,7 +1,7 @@
 const MONGO_URL =
   process.env.MONGO_URL ||
   "mongodb://the_username:the_password@localhost:3456/the_database";
-const REDIS_URL = "redis://localhost:6378" || process.env.REDIS_URL;
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6378";
 
 module.exports = {
   MONGO_URL, //: 'mongodb://the_username:the_password@localhost:3456/the_database',
