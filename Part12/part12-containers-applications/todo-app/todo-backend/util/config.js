54f32f48d1a499e0c05d042f3b7d4e470b104a15
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-06-06 20:10:31 -0700

    Exercise 12.20
    Create a production docker-compose.yml with all of the services, Nginx, todo-backend, todo-frontend, MongoDB and Redis

--- Part12/part12-containers-applications/todo-app/todo-backend/util/config.js
+++ Part12/part12-containers-applications/todo-app/todo-backend/util/config.js
@@ -1,6 +1,6 @@
 const MONGO_URL =
   process.env.MONGO_URL ||
-  "mongodb://the_username:the_password@localhost:3456/the_database";
+  "mongodb://admin:Pa$$pa$$@localhost:3456/prod_database";
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6378";
 
 module.exports = {
