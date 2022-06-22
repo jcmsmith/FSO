9be04cce5744d4cc0eb0ba4bc3a38e85c4777f46
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-06-22 16:15:08 -0700

    Add rollback

--- Part13/util/db.js
+++ Part13/util/db.js
@@ -12,19 +12,6 @@ const sequelize = new Sequelize(DATABASE_URL, {
   },
 });
 
-const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: { glob: "migrations/*.js" },
-    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  });
-  const migrations = await migrator.up();
-  console.log("Migrations up to date", {
-    files: migrations.map((mig) => mig.name),
-  });
-};
-
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
@@ -36,6 +23,27 @@ const connectToDatabase = async () => {
   }
 
   return null;
+};
+
+const migrationConf = {
+  migrations: { glob: "migrations/*.js" },
+  storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
 };
 
 module.exports = { connectToDatabase, sequelize };
